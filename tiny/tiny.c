2cd267ae32610f51f02e899e4641055ecb47c1fe
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-28 16:35:02 +0900

    Solved homework problem 11.10

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -126,29 +126,49 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
 {
   char *ptr;
 
-  if (!strstr(uri, "cgi-bin"))
-  { /* Static content */             // line:netp:parseuri:isstatic
-    strcpy(cgiargs, "");             // line:netp:parseuri:clearcgi
-    strcpy(filename, ".");           // line:netp:parseuri:beginconvert1
-    strcat(filename, uri);           // line:netp:parseuri:endconvert1
-    if (uri[strlen(uri) - 1] == '/') // line:netp:parseuri:slashcheck
-      strcat(filename, "home.html"); // line:netp:parseuri:appenddefault
-    return 1;
-  }
-  else
-  { /* Dynamic content */  // line:netp:parseuri:isdynamic
-    ptr = index(uri, '?'); // line:netp:parseuri:beginextract
-    if (ptr)
+  if (!strstr(uri, "cgi-bin")) /* Static content */
+  {                            // line:netp:parseuri:isstatic
+    strcpy(cgiargs, "");       // line:netp:parseuri:clearcgi
+    strcpy(filename, ".");     // line:netp:parseuri:beginconvert1
+
+    // .html 파일 요청을 처리
+    if (strstr(uri, ".html"))
     {
-      strcpy(cgiargs, ptr + 1);
-      *ptr = '\0';
+      strcat(filename, uri);
+    }
+    else if (uri[strlen(uri) - 1] == '/') // line:netp:parseuri:slashcheck
+    {
+      // URI에 .html이 포함되어 있는 경우
+      strcat(filename, "home.html"); // line:netp:parseuri:appenddefault
     }
     else
-      strcpy(cgiargs, ""); // line:netp:parseuri:endextract
-    strcpy(filename, "."); // line:netp:parseuri:beginconvert2
-    strcat(filename, uri); // line:netp:parseuri:endconvert2
-    return 0;
+    {
+      // 다른 정적 콘텐츠 요청 처리
+      strcat(filename, uri); // line:netp:parseuri:endconvert1
+    }
+
+    return 1;
   }
+
+  /* Dynamic content */
+  // line:netp:parseuri:isdynamic
+  // line:netp:parseuri:beginextract
+  ptr = index(uri, '?');
+
+  if (ptr)
+  {
+    strcpy(cgiargs, ptr + 1);
+    *ptr = '\0';
+  }
+  else
+  {
+    strcpy(cgiargs, ""); // line:netp:parseuri:endextract
+  }
+
+  strcpy(filename, "."); // line:netp:parseuri:beginconvert2
+  strcat(filename, uri); // line:netp:parseuri:endconvert2
+
+  return 0;
 }
 /* $end parse_uri */
 
